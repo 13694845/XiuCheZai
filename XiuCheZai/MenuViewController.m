e744a6728314e1d3640602a7954b31161ed4e0b3
Your Name <you@example.com>
2016-04-19 10:50:48 +0800

    .

--- XiuCheZai/MenuViewController.m
+++ XiuCheZai/MenuViewController.m
@@ -10,8 +10,6 @@
 #import <CoreLocation/CoreLocation.h>
 #import "WebViewController.h"
 #import "Config.h"
-#import "AFNetworking.h"
-#import "MBProgressHUD.h"
 
 @interface MenuViewController ()
 
@@ -34,36 +32,28 @@
     }];
 }
 
-- (void)launchWebViewWithURLString:(NSString *)urlString {
-    WebViewController *webViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"WebViewController"];
-    webViewController.url = [NSURL URLWithString:urlString];
-    webViewController.hidesBottomBarWhenPushed = YES;
-    [self.navigationController pushViewController:webViewController animated:YES];
-    [self close:nil];
-}
-
 - (IBAction)toMenu01:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220201&title=%E4%BF%9D%E9%99%A9%E6%9D%A0"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=130401&title=%E6%9C%BA%E6%B2%B9"]];
 }
 
 - (IBAction)toMenu02:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220205&title=%E4%B8%AD%E7%BD%91"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=130200&title=%E6%BB%A4%E6%B8%85%E5%99%A8"]];
 }
 
 - (IBAction)toMenu03:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220102&title=%E5%8F%91%E5%8A%A8%E6%9C%BA%E7%9B%96"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=180200&title=%E8%BD%AE%E8%83%8E"]];
 }
 
 - (IBAction)toMenu04:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=190302&title=%E5%A4%A7%E7%81%AF"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=270420&title=%E9%9B%A8%E5%88%AE%E7%89%87"]];
 }
 
 - (IBAction)toMenu05:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=190310&title=%E5%B0%BE%E7%81%AF"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=191100&title=%E7%94%B5%E7%93%B6"]];
 }
 
 - (IBAction)toMenu06:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=190304&title=%E9%9B%BE%E7%81%AF"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=110100&title=%E7%81%AB%E8%8A%B1%E5%A1%9E"]];
 }
 
 - (IBAction)toMenu07:(id)sender {
@@ -71,51 +61,59 @@
 }
 
 - (IBAction)toMenu08:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=170500&title=%E5%88%B6%E5%8A%A8%E7%9B%98"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=170500&title=%E5%88%B9%E8%BD%A6%E7%9B%98"]];
 }
 
 - (IBAction)toMenu09:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=110100&title=%E7%81%AB%E8%8A%B1%E5%A1%9E"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=190300&title=%E8%BD%A6%E7%81%AF"]];
 }
 
 - (IBAction)toMenu10:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=180100&title=%E9%92%A2%E5%9C%88"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220200&title=%E8%BD%A6%E8%BA%AB%E4%BB%B6"]];
 }
 
 - (IBAction)toMenu11:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=180200&title=%E8%BD%AE%E8%83%8E"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220100&title=%E9%92%A3%E9%87%91%E4%BB%B6"]];
 }
 
 - (IBAction)toMenu12:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=130200&title=%E6%9C%BA%E6%B2%B9%E6%BB%A4%E8%8A%AF"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=270400&title=%E6%94%B9%E8%A3%85%E4%BB%B6"]];
 }
 
 - (IBAction)toMenu13:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=100601&title=%E7%A9%BA%E6%B0%94%E6%BB%A4%E8%8A%AF"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=100300&title=%E5%BA%95%E7%9B%98%E4%BB%B6"]];
 }
 
 - (IBAction)toMenu14:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=241700&title=%E8%A1%8C%E6%9D%8E%E6%9E%B6"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=250400&title=%E8%BE%85%E6%96%99"]];
 }
 
 - (IBAction)toMenu15:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220204&title=%E9%97%A8%E8%BE%B9%E8%B8%8F%E6%9D%BF"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @""]];
 }
 
 - (IBAction)toMenu16:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220101&title=%E8%BD%A6%E6%9E%B6%E6%80%BB%E6%88%90"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @""]];
 }
 
 - (IBAction)toMenu17:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220206&title=%E5%B0%BE%E7%BF%BC"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @""]];
 }
 
 - (IBAction)toMenu18:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220103&title=%E5%90%8E%E5%A4%87%E7%AE%B1%E7%9B%96"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @""]];
 }
 
 - (IBAction)toMenu19:(id)sender {
-    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/list/category.html?pjfl=220105&title=%E5%8F%B6%E5%AD%90%E6%9D%BF"]];
+    [self launchWebViewWithURLString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @""]];
+}
+
+- (void)launchWebViewWithURLString:(NSString *)URLString {
+    WebViewController *webViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"WebViewController"];
+    webViewController.url = [NSURL URLWithString:URLString];
+    webViewController.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:webViewController animated:YES];
+    [self close:nil];
 }
 
 - (IBAction)close:(id)sender {
