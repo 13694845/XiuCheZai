df5c91e8eca2e172debf23de2085ca80de0b4008
Your Name <you@example.com>
2016-05-09 14:48:37 +0800

    .

--- XiuCheZai/HelloViewController.m
+++ XiuCheZai/HelloViewController.m
@@ -8,8 +8,6 @@
 
 #import "HelloViewController.h"
 
-@import MapKit;
-
 @interface HelloViewController ()
 
 @end
@@ -18,7 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -26,17 +23,6 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
