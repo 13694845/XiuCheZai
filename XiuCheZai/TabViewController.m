63fd7929467065635a3a776f6aaaed7d440a583f
Your Name <you@example.com>
2016-06-17 16:40:30 +0800

    .

--- XiuCheZai/TabViewController.m
+++ XiuCheZai/TabViewController.m
@@ -25,7 +25,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    if (!self.webView.isLoading) [self.webView reload];
+//    if (!self.webView.isLoading) [self.webView reload];
 }
 
 - (void)viewWillLayoutSubviews {
