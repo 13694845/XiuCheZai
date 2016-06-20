50afa288251bbdfb8c5405e2957eb2180b095ae2
Your Name <you@example.com>
2016-06-20 11:18:28 +0800

    .

--- XiuCheZai/HomeViewController.m
+++ XiuCheZai/HomeViewController.m
@@ -207,22 +207,23 @@
 }
 
 - (IBAction)toMenu:(id)sender {
-    /*
     MenuViewController *menuViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"MenuViewController"];
     [self addChildViewController:menuViewController];
     menuViewController.view.backgroundColor = [UIColor colorWithRed:0/255.0 green:0/255.0 blue:0/255.0 alpha:0.6];
     CGRect rect = menuViewController.view.frame;
     rect.origin.x -= rect.size.width;
+    rect.size.height = [UIScreen mainScreen].bounds.size.height - self.tabBarController.tabBar.bounds.size.height;
     menuViewController.view.frame = rect;
     [self.view addSubview:menuViewController.view];
     rect.origin.x = 0;
     [UIView animateWithDuration:0.3 animations:^{
         menuViewController.view.frame = rect;
     }];
-     */
+    /*
     MenuViewController *menuViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"MenuViewController"];
     menuViewController.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
     [self presentViewController:menuViewController animated:YES completion:nil];
+     */
 }
 
 - (IBAction)toScanner:(id)sender {
