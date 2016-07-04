cebfa7268657e2e5e7381afc1712ddc6c7d0c8a1
Your Name <you@example.com>
2016-07-04 08:44:47 +0800

    .

--- XiuCheZai/AppDelegate.h
+++ XiuCheZai/AppDelegate.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 #import "WXApi.h"
 
+#import <BaiduMapAPI_Base/BMKBaseComponent.h>
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
