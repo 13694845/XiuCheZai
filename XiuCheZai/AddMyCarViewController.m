d60ab96174c453bb5052f0aa8eca7d434e690036
Your Name <you@example.com>
2016-07-13 16:09:22 +0800

    .

--- XiuCheZai/AddMyCarViewController.m
+++ XiuCheZai/AddMyCarViewController.m
@@ -10,6 +10,15 @@
 
 @interface AddMyCarViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *modelTextField;
+@property (weak, nonatomic) IBOutlet UITextField *ownerTextField;
+@property (weak, nonatomic) IBOutlet UITextField *telTextField;
+@property (weak, nonatomic) IBOutlet UITextField *plateNoTextField;
+@property (weak, nonatomic) IBOutlet UITextField *registerDateTextField;
+@property (weak, nonatomic) IBOutlet UITextField *mileageTextField;
+@property (weak, nonatomic) IBOutlet UITextField *vinTextField;
+@property (weak, nonatomic) IBOutlet UITextField *engineNoTextField;
+
 @end
 
 @implementation AddMyCarViewController
@@ -21,10 +30,18 @@
     
     self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName:[UIColor whiteColor]};
     self.navigationItem.title = @"添加车型";
+    
+    // UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"common_back.png"] style:UIBarButtonItemStylePlain target:self action:@selector(scan)];
+    UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"行驶证" style:UIBarButtonItemStylePlain target:self action:@selector(scan)];
+    self.navigationItem.rightBarButtonItem = barButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (void)scan {
+    NSLog(@"scan");
 }
 
 @end
