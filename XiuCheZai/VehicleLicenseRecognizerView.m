f57d952c3fe147f7a2196d821ca544c57bb0a466
Your Name <you@example.com>
2016-07-14 08:11:30 +0800

    .

---
+++ XiuCheZai/VehicleLicenseRecognizerView.m
@@ -0,0 +1,13 @@
+//
+//  VehicleLicenseRecognizerView.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/7/14.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "VehicleLicenseRecognizerView.h"
+
+@implementation VehicleLicenseRecognizerView
+
+@end
