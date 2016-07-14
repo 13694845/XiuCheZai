ecf2548df3881fae6e11f1e34eb4ce70e7e3b68f
Your Name <you@example.com>
2016-07-14 14:18:31 +0800

    .

--- XiuCheZai/BrandSelectorViewController.m
+++ XiuCheZai/BrandSelectorViewController.m
@@ -8,15 +8,28 @@
 
 #import "BrandSelectorViewController.h"
 #import "BrandSelectorCell.h"
+#import "Config.h"
+#import "AFNetworking.h"
 
 @interface BrandSelectorViewController () <UITableViewDataSource, UITableViewDelegate>
 
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) AFHTTPSessionManager *manager;
 @property (strong, nonatomic) NSMutableArray *brands;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation BrandSelectorViewController
+
+- (AFHTTPSessionManager *)manager {
+    if (!_manager) {
+        _manager = [AFHTTPSessionManager manager];
+        [_manager.requestSerializer setValue:[NSString stringWithFormat:@"%@ %@/%@",
+                                              [_manager.requestSerializer valueForHTTPHeaderField:@"User-Agent"], @"APP8673h", [Config version]] forHTTPHeaderField:@"User-Agent"];
+    }
+    return _manager;
+}
 
 - (NSMutableArray *)brands {
     if (!_brands) _brands = [NSMutableArray array];
@@ -33,7 +46,20 @@
 }
 
 - (void)loadData {
-    
+    NSString *URLString = [NSString stringWithFormat:@"%@%@", [Config baseURL], @"/Action/LoadCarBrandData.do"];
+    NSDictionary *parameters = @{@"type":@"1"};
+    [self.manager POST:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        NSDictionary *data = responseObject[@"data"];
+        
+        for (int i = 'A'; i < 'Z'; i++) {
+            
+        }
+        
+        
+        NSArray *brands = data[@"A"];
+        NSLog(@"brand A : %@", brands);
+        
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {}];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
