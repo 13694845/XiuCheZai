a6d6feabf513fd8dfbaa1fe86b4f36726a98979b
Your Name <you@example.com>
2016-07-25 14:51:15 +0800

    .

--- XiuCheZai/NewHomeViewController.m
+++ XiuCheZai/NewHomeViewController.m
@@ -21,6 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.tabBarController.tabBar.tintColor = [UIColor colorWithRed:229.0/255.0 green:21.0/255.0 blue:45.0/255.0 alpha:1.0];
+    self.scrollView.showsVerticalScrollIndicator = NO;
     self.scrollView.contentSize = self.mainView.bounds.size;
     self.newsView.layer.cornerRadius = 3.5;
 }
