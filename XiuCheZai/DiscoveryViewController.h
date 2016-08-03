eb08247aa8cb8e3136bd852de9f8950185ef5eb0
Your Name <you@example.com>
2016-08-03 09:45:52 +0800

    .

---
+++ XiuCheZai/DiscoveryViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiscoveryViewController.h
+//  XiuCheZai
+//
+//  Created by QSH on 16/8/3.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "TabViewController.h"
+
+@interface DiscoveryViewController : TabViewController
+
+@end
---
+++ XiuCheZai/DiscoveryViewController.m
@@ -0,0 +1,31 @@
+//
+//  DiscoveryViewController.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/8/3.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "DiscoveryViewController.h"
+#import "Config.h"
+
+@interface DiscoveryViewController ()
+
+@end
+
+@implementation DiscoveryViewController
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    self.url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/community/index/"]];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
