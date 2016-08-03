8a7cd5bed91f43816b423c56d8e0b8da5ee222e2
Your Name <you@example.com>
2016-08-03 14:10:30 +0800

    .

--- XiuCheZai/TabViewController.m
+++ XiuCheZai/TabViewController.m
@@ -45,6 +45,8 @@
 
 - (BOOL)handleNavigationWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     self.fullScreen = ![request.URL.description containsString:self.url.description];
+    if ([self.url.description containsString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/community/index/"]]) self.fullScreen = YES;
+    
     [self viewWillLayoutSubviews];
     self.needsRefresh = !([request.URL.description containsString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/m-center/save_info/index.html"]]
                         || [request.URL.description containsString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/m-center/refund/index.html?orderId="]]
