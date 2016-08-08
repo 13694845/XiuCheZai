d98c77128f84a5ab9cd2c59b34b5265a2c4ac2d0
Your Name <you@example.com>
2016-08-08 14:05:34 +0800

    .

--- XiuCheZai/MenuViewController.m
+++ XiuCheZai/MenuViewController.m
@@ -29,7 +29,7 @@
     CLGeocoder *geocoder = [[CLGeocoder alloc] init];
     [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> *placemarks, NSError *error) {
         CLPlacemark *placemark = placemarks.firstObject;
-        self.cityLabel.text = [NSString stringWithFormat:@"%@ - %@", placemark.locality ? : @"", placemark.subLocality ? : @""];
+        self.cityLabel.text = [NSString stringWithFormat:@"%@", placemark.locality ? : @""];
     }];
 }
 
