23e1e02fb19840ef7c608c910f2f591a981d3bae
Your Name <you@example.com>
2016-08-19 17:25:02 +0800

    .

--- XiuCheZai/XCZDiscoveryViewController.m
+++ XiuCheZai/XCZDiscoveryViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "XCZDiscoveryViewController.h"
+#import "XCZNewsViewController.h"
 
 @interface XCZDiscoveryViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+
+@property (nonatomic, strong) XCZNewsViewController *newsViewController;
 
 @end
 
@@ -17,6 +22,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.tabBarController.tabBar.tintColor = [UIColor colorWithRed:229.0/255.0 green:21.0/255.0 blue:45.0/255.0 alpha:1.0];
+    
+    self.newsViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"XCZNewsViewController"];
+    [self addChildViewController:self.newsViewController];
+    self.newsViewController.view.frame = self.contentView.frame;
+    [self.view addSubview:self.newsViewController.view];
 }
 
 - (void)didReceiveMemoryWarning {
