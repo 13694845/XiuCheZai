5b396ba9f762e4fcd3c19ce63da4042aa01a6654
Your Name <you@example.com>
2016-08-19 16:47:26 +0800

    .

---
+++ XiuCheZai/XCZNewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  XCZNewsViewController.h
+//  XiuCheZai
+//
+//  Created by QSH on 16/8/19.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCZNewsViewController : UIViewController
+
+@end
---
+++ XiuCheZai/XCZNewsViewController.m
@@ -0,0 +1,25 @@
+//
+//  XCZNewsViewController.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/8/19.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "XCZNewsViewController.h"
+
+@interface XCZNewsViewController ()
+
+@end
+
+@implementation XCZNewsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
