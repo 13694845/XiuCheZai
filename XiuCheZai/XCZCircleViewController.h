8b3cb824a5ef08a35d204c951e827356151989b3
Your Name <you@example.com>
2016-08-22 09:02:20 +0800

    .

---
+++ XiuCheZai/XCZCircleViewController.h
@@ -0,0 +1,13 @@
+//
+//  XCZCircleViewController.h
+//  XiuCheZai
+//
+//  Created by QSH on 16/8/22.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCZCircleViewController : UIViewController
+
+@end
---
+++ XiuCheZai/XCZCircleViewController.m
@@ -0,0 +1,25 @@
+//
+//  XCZCircleViewController.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/8/22.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "XCZCircleViewController.h"
+
+@interface XCZCircleViewController ()
+
+@end
+
+@implementation XCZCircleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
