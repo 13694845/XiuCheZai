62803c76d7f1b66593800d2d27272f513dd05961
Your Name <you@example.com>
2016-08-26 09:41:22 +0800

    .

--- XiuCheZai/ScannerViewController.m
+++ XiuCheZai/ScannerViewController.m
@@ -37,8 +37,7 @@
     CGFloat const kBoxHeight = 200.0;
     
     // UIView *boxView = [[UIView alloc] initWithFrame:CGRectMake(60.0, 110.0, 200.0, 200.0)];
-    CGRect screen = [UIScreen mainScreen].bounds;
-    UIView *boxView = [[UIView alloc] initWithFrame:CGRectMake((screen.size.width-kBoxWidth)/2.0, (screen.size.height-kBoxHeight)/2.0, kBoxWidth, kBoxHeight)];
+    UIView *boxView = [[UIView alloc] initWithFrame:CGRectMake(([UIScreen mainScreen].bounds.size.width-kBoxWidth)/2.0, 120.0, kBoxWidth, kBoxHeight)];
 
     boxView.layer.borderColor = [UIColor greenColor].CGColor;
     boxView.layer.borderWidth = 1.0;
