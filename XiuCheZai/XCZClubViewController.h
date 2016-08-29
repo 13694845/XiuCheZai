6e62318a8a628a7fbfe3d5f1fef3c5c783c22503
Your Name <you@example.com>
2016-08-29 13:47:06 +0800

    .

---
+++ XiuCheZai/XCZClubViewController.h
@@ -0,0 +1,13 @@
+//
+//  XCZClubViewController.h
+//  XiuCheZai
+//
+//  Created by QSH on 16/8/29.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "XCZContentViewController.h"
+
+@interface XCZClubViewController : XCZContentViewController
+
+@end
---
+++ XiuCheZai/XCZClubViewController.m
@@ -0,0 +1,25 @@
+//
+//  XCZClubViewController.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/8/29.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "XCZClubViewController.h"
+
+@interface XCZClubViewController ()
+
+@end
+
+@implementation XCZClubViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
