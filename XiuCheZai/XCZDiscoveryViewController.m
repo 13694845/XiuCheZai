99ad126739febc3763db87dbc9dbc9731ab0fecd
Your Name <you@example.com>
2016-08-29 08:26:54 +0800

    .

--- XiuCheZai/XCZDiscoveryViewController.m
+++ XiuCheZai/XCZDiscoveryViewController.m
@@ -36,8 +36,7 @@ typedef NS_OPTIONS(NSUInteger, DiscoveryContentTransition) {
     [self addChildViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"XCZCircleViewController"]];
     
     [self.buttons.firstObject setAlpha:1.0];
-    // UIViewController *viewController = self.childViewControllers.firstObject;
-    UIViewController *viewController = self.childViewControllers[1];
+    UIViewController *viewController = self.childViewControllers.firstObject;
     viewController.view.frame = self.contentView.bounds;
     [self.contentView addSubview:viewController.view];
 }
