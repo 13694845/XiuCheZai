ee51b750c2beca11151a10d46ee933f8ab8ba82c
Your Name <you@example.com>
2016-08-30 12:51:51 +0800

    .

--- XiuCheZai/LoginViewController.m
+++ XiuCheZai/LoginViewController.m
@@ -19,15 +19,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    /*
     NSString *publicKey = @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCIyirbChVxQFk3n5ZDyBksvMEmdDIWM+52iGIgItINV0ivasC2MpE1OzFzwgLt2nv14LXJTRmawLf1cduRhVWT13ldhidL601KE23Wabo30TKNJmMR0gLPD2PTq5JjmuwxSEd5AIdGm3OIaRrScQ24PlEbho2+ApTLjzCknGkY1wIDAQAB";
-    */
-    
+    /*
     NSString *publicKey = @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCZFhRlr0UbL+xuLr2Z8kRR+rGyk4bR3UL40r+Y7fkrlm0RAUvyw2VTOJTkXbMi/oGoT1jEr8AZdNH4rUecGV0SYDac3WzHvTyx/xShDwD30s4z6yTOHSCcgkKaqQXHjnpXGQAGzorI1BwDu47gcVRKGgFvRfKhtci1g5kZIu0f1QIDAQAB";
-
+     */
     
     RSADataEncryptor *encryptor = [[RSADataEncryptor alloc] initWithPublicKey:publicKey];
-    NSString * encryptedString = [encryptor encryptString:@"password"];
+    NSString * encryptedString = [encryptor encryptString:@"密码"];
     NSLog(@"encryptedString : %@", encryptedString);
     
     BMKMapView *mapView = [[BMKMapView alloc] initWithFrame:CGRectMake(0, 0, 320.0, 480.0)];
