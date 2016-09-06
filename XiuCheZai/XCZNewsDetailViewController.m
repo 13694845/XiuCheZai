e81dae25369bb5f4acfd44557290f787e80e7b8c
Your Name <you@example.com>
2016-09-06 13:40:11 +0800

    .

--- XiuCheZai/XCZNewsDetailViewController.m
+++ XiuCheZai/XCZNewsDetailViewController.m
@@ -7,15 +7,32 @@
 //
 
 #import "XCZNewsDetailViewController.h"
+#import "XCZNewsDetailModel.h"
 
 @interface XCZNewsDetailViewController ()
+
+@property (nonatomic, strong) XCZNewsDetailModel *newsDetailModel;
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, strong) UIView *contentView;
 
 @end
 
 @implementation XCZNewsDetailViewController
 
+- (XCZNewsDetailModel *)newsDetailModel {
+    if (_newsDetailModel) _newsDetailModel = [[XCZNewsDetailModel alloc] init];
+    return _newsDetailModel;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 700)];
+    
+    // ...
+    
+    self.scrollView.contentSize = self.contentView.bounds.size;
+    [self.scrollView addSubview:self.contentView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
