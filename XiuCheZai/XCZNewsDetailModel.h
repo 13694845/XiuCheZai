527d914b5fd9252b86e576bd6497434ed36b546a
Your Name <you@example.com>
2016-09-06 12:49:18 +0800

    .

---
+++ XiuCheZai/XCZNewsDetailModel.h
@@ -0,0 +1,13 @@
+//
+//  XCZNewsDetailModel.h
+//  XiuCheZai
+//
+//  Created by QSH on 16/9/6.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XCZNewsDetailModel : NSObject
+
+@end
---
+++ XiuCheZai/XCZNewsDetailModel.m
@@ -0,0 +1,13 @@
+//
+//  XCZNewsDetailModel.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/9/6.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "XCZNewsDetailModel.h"
+
+@implementation XCZNewsDetailModel
+
+@end
