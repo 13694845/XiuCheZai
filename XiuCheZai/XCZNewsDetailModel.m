76cfd57a1c21a151ef56c3fba7970972b89a0cf9
Your Name <you@example.com>
2016-09-06 13:25:12 +0800

    .

--- XiuCheZai/XCZNewsDetailModel.m
+++ XiuCheZai/XCZNewsDetailModel.m
@@ -14,12 +14,35 @@
 @property (nonatomic, strong) NSString *publishDate;
 @property (nonatomic, strong) NSString *reprintFrom;
 @property (nonatomic, strong) NSString *newsText;
-@property (nonatomic, strong) NSString *declaration;
 @property (nonatomic, strong) NSArray *admiredPersons;
 @property (nonatomic, strong) NSArray *newsRemarks;
 
 @end
 
 @implementation XCZNewsDetailModel
+
+- (NSString *)newsTitle {
+    return @"文章标题文章标题文章标题题文章标题题文章标题";
+}
+
+- (NSString *)publishDate {
+    return @"昨天 14：36";
+}
+
+- (NSString *)reprintFrom {
+    return @"新浪汽车";
+}
+
+- (NSString *)newsText {
+    return @"<h1>news text here.</h1>";
+}
+
+- (NSArray *)admiredPersons {
+    return nil;
+}
+
+- (NSArray *)newsRemarks {
+    return nil;
+}
 
 @end
