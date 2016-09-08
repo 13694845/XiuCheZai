b786a5d69c73617513ab26330a3f52f842607616
Your Name <you@example.com>
2016-09-08 09:35:05 +0800

    .

--- XiuCheZai/XCZNewsTableViewCell.m
+++ XiuCheZai/XCZNewsTableViewCell.m
@@ -10,15 +10,22 @@
 
 @interface XCZNewsTableViewCell ()
 
+/*
 @property (weak, nonatomic) IBOutlet NSString *newsTitle;
 @property (weak, nonatomic) IBOutlet NSString *publishDate;
 @property (weak, nonatomic) IBOutlet NSString *reprintFrom;
 @property (weak, nonatomic) IBOutlet NSString *newsText;
 @property (weak, nonatomic) IBOutlet NSArray *admiredPersons;
 @property (weak, nonatomic) IBOutlet NSArray *newsRemarks;
+*/
+
+@property (weak, nonatomic) IBOutlet UILabel *newsTitleLabel;
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *newsImageViews;
 
 
-
+@property (weak, nonatomic) IBOutlet UILabel *reprintFromLabel;
+@property (weak, nonatomic) IBOutlet UILabel *remarkCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *praiseCountLabel;
 
 @end
 
@@ -26,6 +33,9 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
+    
+    // self.reuseIdentifier = @"CellA"
+    NSLog(@"");
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
