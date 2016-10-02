58e77465082814c1bb04d3e3e12bac16d22494ed
Your Name <you@example.com>
2016-10-02 10:43:03 +0800

    .

--- XiuCheZai/icon/ChatViewController.h
+++ XiuCheZai/icon/ChatViewController.h
@@ -8,10 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+/*
 typedef NS_ENUM(NSUInteger, ChatActionType) {
     ChatActionTypeTest,
     ChatActionTypeLogin
 };
+*/
 
 @interface ChatViewController : UIViewController
 
--- XiuCheZai/icon/ChatViewController.m
+++ XiuCheZai/icon/ChatViewController.m
@@ -19,10 +19,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
     [self setupSocket];
     [self conn];
-    [self send];
-    [self login];
+    // [self send];
+    // [self loginWithUserId:@"555"];
+    
+    // [self sendMessageFromSender:@{@"sender_id":@"555", @"sender_name":@"zhangsan"} toReceiver:@{@"receiver_id":@"123", @"receiver_name":@"lisi"} withContent:@"content" type:@"txt"];
+    [self historyMessagesForSenderId:@"555" receiverId:@"123" sendTime:@"2016-10-10" page:@"1"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -71,27 +77,38 @@
     NSString *msg = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSLog(@"didReadData : %@", msg);
     
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+    // NSLog(@"json : %@", json);
     
+    NSString *type = json[@"type"];
+    NSLog(@"type : %@", type);
     
-    
-    
-    // if (query) parameter = [NSJSONSerialization JSONObjectWithData:[query dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableLeaves error:nil];
-    
+    if ([type isEqualToString:@"LOGIN"]) {
+        NSLog(@"LOGIN : %@", json[@"content"]);
+    }
 }
 
+- (void)loginWithUserId:(NSString *)userId {
+    NSString *message = [NSString stringWithFormat:@"{\"type\":\"LOGIN\", \"sender_id\":\"%@\"}\n", userId];
+    [asyncSocket writeData:[message dataUsingEncoding:NSUTF8StringEncoding] withTimeout:-1.0 tag:0];
+    NSData *terminatorData = [@"\n" dataUsingEncoding:NSASCIIStringEncoding];
+    [asyncSocket readDataToData:terminatorData withTimeout:-1.0 tag:0];
+}
 
+- (void)sendMessageFromSender:(NSDictionary *)sender toReceiver:(NSDictionary *)receiver withContent:(NSString *)content type:(NSString *)type {
+    NSString *messageFormat = @"{\"type\":\"MESSAGE\", \"sender_id\":\"%@\", \"receiver_id\":\"%@\", \"sender_name\":\"%@\", \"receiver_name\":\"%@\", \"msg_content\":\"%@\", \"msg_type\":\"%@\", \"play_time\":\"%@\"}\n";
+    NSString *message = [NSString stringWithFormat:messageFormat, sender[@"sender_id"], receiver[@"receiver_id"], sender[@"sender_name"], receiver[@"receiver_name"], content, type, @"-1"];
+    NSLog(@"sendMessage : %@", message);
+    [asyncSocket writeData:[message dataUsingEncoding:NSUTF8StringEncoding] withTimeout:-1.0 tag:0];
+    NSData *terminatorData = [@"\n" dataUsingEncoding:NSASCIIStringEncoding];
+    [asyncSocket readDataToData:terminatorData withTimeout:-1.0 tag:0];
+}
 
-
-
-
-- (void)login {
-    NSString *msg = @"{\"type\":\"LOGIN\", \"sender_id\":\"555\"}\n";
-    
-    NSString *requestStr = msg;
-    NSData *requestData = [requestStr dataUsingEncoding:NSUTF8StringEncoding];
-    [asyncSocket writeData:requestData withTimeout:-1.0 tag:0];
-    
-    // [asyncSocket readDataWithTimeout:-1.0 tag:0];
+- (void)historyMessagesForSenderId:(NSString *)senderId receiverId:(NSString *)receiverId sendTime:(NSString *)sendTime page:(NSString *)page {
+    NSString *messageFormat = @"{\"type\":\"CHATHISTORY\", \"sender_id\":\"%@\", \"receiver_id\":\"%@\", \"send_time\":\"%@\", \"NowPage\":\"%@\"}\n";
+    NSString *message = [NSString stringWithFormat:messageFormat, senderId, receiverId, sendTime, page];
+    NSLog(@"historyMessages : %@", message);
+    [asyncSocket writeData:[message dataUsingEncoding:NSUTF8StringEncoding] withTimeout:-1.0 tag:0];
     NSData *terminatorData = [@"\n" dataUsingEncoding:NSASCIIStringEncoding];
     [asyncSocket readDataToData:terminatorData withTimeout:-1.0 tag:0];
 }
