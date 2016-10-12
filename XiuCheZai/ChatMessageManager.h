f7b870eeb67cacb69a0008e351f983041c96d617
Your Name <you@example.com>
2016-10-12 09:23:15 +0800

    .

---
+++ XiuCheZai/ChatMessageManager.h
@@ -0,0 +1,15 @@
+//
+//  ChatMessageManager.h
+//  XiuCheZai
+//
+//  Created by QSH on 16/10/12.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChatMessageManager : NSObject
+
++ (instancetype)sharedManager;
+
+@end
---
+++ XiuCheZai/ChatMessageManager.m
@@ -0,0 +1,22 @@
+//
+//  ChatMessageManager.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/10/12.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "ChatMessageManager.h"
+
+@implementation ChatMessageManager
+
++ (instancetype)sharedManager {
+    static ChatMessageManager *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
+
+@end
