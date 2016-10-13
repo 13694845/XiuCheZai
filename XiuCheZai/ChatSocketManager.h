d486540244104c611356aee370cda350d518363d
Your Name <you@example.com>
2016-10-13 14:43:04 +0800

    .

---
+++ XiuCheZai/ChatSocketManager.h
@@ -0,0 +1,13 @@
+//
+//  ChatSocketManager.h
+//  XiuCheZai
+//
+//  Created by QSH on 16/10/13.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChatSocketManager : NSObject
+
+@end
---
+++ XiuCheZai/ChatSocketManager.m
@@ -0,0 +1,13 @@
+//
+//  ChatSocketManager.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/10/13.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "ChatSocketManager.h"
+
+@implementation ChatSocketManager
+
+@end
