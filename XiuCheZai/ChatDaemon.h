1ae2d9745e5aeaff086b150ebee17f9acd61710f
Your Name <you@example.com>
2016-10-13 15:14:12 +0800

    .

---
+++ XiuCheZai/ChatDaemon.h
@@ -0,0 +1,13 @@
+//
+//  ChatDaemon.h
+//  XiuCheZai
+//
+//  Created by QSH on 16/10/13.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChatDaemon : NSObject
+
+@end
---
+++ XiuCheZai/ChatDaemon.m
@@ -0,0 +1,13 @@
+//
+//  ChatDaemon.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/10/13.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "ChatDaemon.h"
+
+@implementation ChatDaemon
+
+@end
