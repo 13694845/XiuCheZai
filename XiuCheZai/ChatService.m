a2d5e81a14fd4170e6a447cb381fd2a2e1286ffd
Your Name <you@example.com>
2016-10-14 08:47:13 +0800

    .

--- XiuCheZai/ChatService.m
+++ XiuCheZai/ChatService.m
@@ -28,6 +28,7 @@
 
 @implementation ChatService
 
+
 /*
 - (instancetype)init {
     if (self = [super init]) {
@@ -36,8 +37,6 @@
         self.senderName = @"zhangsan";
         self.receiverId = @"123";
         self.receiverName = @"lisi";
-        
-
     }
     return self;
 }
@@ -45,15 +44,19 @@
 
 
 
-
 - (void)start {
     NSLog(@"start");
     
-    [self startHeartbeat];
-    /*
+    self.senderId = @"555";
+    self.senderName = @"zhangsan";
+    self.receiverId = @"123";
+    self.receiverName = @"lisi";
+
+    
+    
+    
     if (!self.asyncSocket) [self setupSocket];
     [self connectToHost:HOST onPort:PORT];
-     */
 }
 
 - (void)stop {
@@ -61,9 +64,36 @@
 
 }
 
+
+- (void)setupSocket {
+    NSLog(@"setupSocket");
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+    self.asyncSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:mainQueue];
+    
+    if (!self.asyncSocket) [self setupSocket];
+    [self connectToHost:HOST onPort:PORT];
+}
+
+- (void)connectToHost:(NSString *)host onPort:(uint16_t)port {
+    NSLog(@"connectToHost");
+    
+    [self.asyncSocket disconnect];
+    
+    NSError *error = nil;
+    if (![self.asyncSocket connectToHost:host onPort:port error:&error]) {
+        NSLog(@"connectToHost : %@", error);
+    }
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port {
+    NSLog(@"didConnectToHost");
+    [self loginWithSenderId:self.senderId];
+    [self.asyncSocket readDataToData:[TERMINATOR dataUsingEncoding:NSASCIIStringEncoding] withTimeout:-1.0 tag:0];
+}
+
 - (void)startHeartbeat {
     NSLog(@"startHeartbeat");
-    if (!self.timer.valid) self.timer = [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(echo) userInfo:nil repeats:YES];
+    if (!self.timer.valid) self.timer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(echo) userInfo:nil repeats:YES];
 }
 
 - (void)stopHeartbeat {
@@ -72,37 +102,9 @@
 }
 
 - (void)echo {
-    NSLog(@"AppDelegate : %@", [NSString stringWithFormat:@"{\"type\":\"ECHO\"}\n"]);
-}
-
-
-
-
-
-
-
-- (void)setupSocket {
-    NSLog(@"setupSocket");
-    dispatch_queue_t mainQueue = dispatch_get_main_queue();
-    self.asyncSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:mainQueue];
-
-    if (!self.asyncSocket) [self setupSocket];
-    [self connectToHost:HOST onPort:PORT];
-}
-
-- (void)connectToHost:(NSString *)host onPort:(uint16_t)port {
-    NSLog(@"connectToHost");
-    NSError *error = nil;
-    if (![self.asyncSocket connectToHost:host onPort:port error:&error]) {
-        NSLog(@"connectToHost : %@", error);
-    }
-}
-
-- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port {
-    // [self setupHeartbeat];
-    NSLog(@"didConnectToHost");
-
-    [self loginWithSenderId:self.senderId];
+    NSLog(@"echo");
+    NSString *message = [NSString stringWithFormat:@"{\"type\":\"ECHO\"}\n"];
+    [self.asyncSocket writeData:[message dataUsingEncoding:NSUTF8StringEncoding] withTimeout:-1.0 tag:0];
     [self.asyncSocket readDataToData:[TERMINATOR dataUsingEncoding:NSASCIIStringEncoding] withTimeout:-1.0 tag:0];
 }
 
@@ -111,6 +113,54 @@
     NSString *message = [NSString stringWithFormat:@"{\"type\":\"LOGIN\", \"sender_id\":\"%@\"}\n", senderId];
     [self.asyncSocket writeData:[message dataUsingEncoding:NSUTF8StringEncoding] withTimeout:-1.0 tag:0];
     [self.asyncSocket readDataToData:[TERMINATOR dataUsingEncoding:NSASCIIStringEncoding] withTimeout:-1.0 tag:0];
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag {
+    NSDictionary *message = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+    NSString *type = message[@"type"];
+    if ([type isEqualToString:@"LOGIN"]) {
+        [self handleLogin:message]; return;
+    }
+    if ([type isEqualToString:@"MESSAGE"]) {
+        [self handleMessage:message]; return;
+    }
+    if ([type isEqualToString:@"ECHO"]) {
+        [self handleEcho:message]; return;
+    }
+    NSLog(@"ERROR : %@", message);
+}
+
+- (void)handleLogin:(NSDictionary *)message {
+    NSLog(@"handleLogin %@ : ", message);
+    [self startHeartbeat];
+    [self.asyncSocket readDataToData:[TERMINATOR dataUsingEncoding:NSASCIIStringEncoding] withTimeout:-1.0 tag:0];
+}
+
+- (void)handleMessage:(NSDictionary *)message {
+    NSLog(@"handleMessage %@ : ", message);
+    /*
+    NSDictionary *msg = [NSJSONSerialization JSONObjectWithData:[message[@"msg"] dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableLeaves error:nil];
+    ChatMessage *chatMessage = [[ChatMessage alloc] init];
+    chatMessage.isSend = NO;
+    chatMessage.type = msg[@"msg_type"];
+    chatMessage.content = msg[@"msg_content"];
+    chatMessage.playTime = msg[@"play_time"];
+    chatMessage.senderTime = msg[@"send_time"];
+    chatMessage.senderId = msg[@"sender_id"];
+    chatMessage.senderName = msg[@"sender_name"];
+    chatMessage.receiverId = msg[@"receiver_id"];
+    chatMessage.receiverName = msg[@"receiver_name"];
+    [[ChatMessageManager sharedManager] saveMessage:chatMessage withReceiverId:self.receiverId];
+    [self.rows addObject:chatMessage];
+    NSLog(@"RECV : %@", msg[@"msg_content"]);
+    
+    [self.tableView reloadData];
+    [self.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:self.rows.count - 1 inSection:0] atScrollPosition:UITableViewScrollPositionNone animated:YES];
+     */
+}
+
+- (void)handleEcho:(NSDictionary *)message {
+    NSLog(@"handleEcho %@ : ", message);
 }
 
 @end
