7b59a8c3e6022079a9b81243f4448282d390aa6e
Your Name <you@example.com>
2016-10-19 10:15:05 +0800

    .

--- XiuCheZai/icon/ChatViewController.m
+++ XiuCheZai/icon/ChatViewController.m
@@ -54,6 +54,7 @@ typedef NS_ENUM(NSUInteger, InputViewType) {
 @property (weak, nonatomic) IBOutlet UIButton *voiceButton;
 @property (weak, nonatomic) IBOutlet UIButton *emotionButton;
 @property (weak, nonatomic) IBOutlet UIButton *othersButton;
+@property (strong, nonatomic) UIButton *recordVoiceButton;
 
 @property (strong, nonatomic) AFHTTPSessionManager *manager;
 @property (strong, nonatomic) GCDAsyncSocket *asyncSocket;
