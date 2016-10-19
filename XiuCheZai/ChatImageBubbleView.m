44c87927d4290aa40a7b9fa0ebc8cac43c43e3e7
Your Name <you@example.com>
2016-10-19 13:54:18 +0800

    .

---
+++ XiuCheZai/ChatImageBubbleView.m
@@ -0,0 +1,13 @@
+//
+//  ChatImageBubbleView.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/10/19.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "ChatImageBubbleView.h"
+
+@implementation ChatImageBubbleView
+
+@end
