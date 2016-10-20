8850389dc10285f17921e3e53bb7aee46e6fdfde
Your Name <you@example.com>
2016-10-20 09:21:24 +0800

    .

--- XiuCheZai/Chat/TestChatViewController.m
+++ XiuCheZai/Chat/TestChatViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "TestChatViewController.h"
+#import "ChatViewController.h"
 
 @interface TestChatViewController ()
-
 
 @property (weak, nonatomic) IBOutlet UITextField *senderId;
 @property (weak, nonatomic) IBOutlet UITextField *senderName;
@@ -25,10 +25,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:229.0/255.0 green:21.0/255.0 blue:45.0/255.0 alpha:1.0];
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    // self.navigationController.navigationBar.translucent = NO;
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName:[UIColor whiteColor]};
+
 }
 
 - (IBAction)testChat:(id)sender {
+    ChatViewController *chatViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"ChatViewController"];
+    chatViewController.senderId = self.senderId.text;
+    chatViewController.senderName = self.senderName.text;
+    chatViewController.senderAvatar = self.senderAvatar.text;
+
+    chatViewController.receiverId = self.receiverId.text;
+    chatViewController.receiverName = self.receiverName.text;
+    chatViewController.receiverAvatar = self.receiverAvatar.text;
     
+    [self.navigationController pushViewController:chatViewController animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
