822cc1e47c753377e8503a5b6e045af4fc5e4799
Your Name <you@example.com>
2016-10-24 13:00:15 +0800

    .

--- XiuCheZai/Chat/ChatService.m
+++ XiuCheZai/Chat/ChatService.m
@@ -42,28 +42,22 @@
     NSDictionary *parameters = nil;
     [self.manager POST:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
         NSLog(@"LoginDetectionAction.do : %@", responseObject);
-        if ([[responseObject objectForKey:@"statu"] isEqualToString:@"0"]) {
-            NSString *URLString = [NSString stringWithFormat:@"%@%@", [XCZConfig baseURL], @"/Action/ContactServlet.do"];
+        if (![[responseObject objectForKey:@"statu"] isEqualToString:@"0"]) return;
+        NSString *URLString = [NSString stringWithFormat:@"%@%@", [XCZConfig baseURL], @"/Action/ContactServlet.do"];
+        NSDictionary *parameters = nil;
+        [self.manager POST:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+            NSLog(@"ContactServlet.do : %@", responseObject);
+            self.senderId = [[[responseObject objectForKey:@"data"] firstObject] objectForKey:@"user_id"];
+            NSString *URLString = [NSString stringWithFormat:@"%@%@", [XCZConfig baseURL], @"/Action/ContactChannelNumServlet.do"];
             NSDictionary *parameters = nil;
             [self.manager POST:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
-                NSLog(@"ContactServlet.do : %@", responseObject);
-                self.senderId = [[[responseObject objectForKey:@"data"] firstObject] objectForKey:@"user_id"];
-                NSString *URLString = [NSString stringWithFormat:@"%@%@", [XCZConfig baseURL], @"/Action/ContactChannelNumServlet.do"];
-                NSDictionary *parameters = nil;
-                [self.manager POST:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
-                    NSLog(@"ContactChannelNumServlet.do : %@", responseObject);
-                    self.host = [responseObject objectForKey:@"data"];
-                    
-                    NSLog(@"%@ | %@", self.senderId, self.host);
-                    
-                    
-                    
-                } failure:^(NSURLSessionDataTask *task, NSError *error) {}];
+                NSLog(@"ContactChannelNumServlet.do : %@", responseObject);
+                self.host = [responseObject objectForKey:@"data"];
+                NSLog(@"%@ | %@", self.senderId, self.host);
             } failure:^(NSURLSessionDataTask *task, NSError *error) {}];
-        }
+        } failure:^(NSURLSessionDataTask *task, NSError *error) {}];
     } failure:^(NSURLSessionDataTask *task, NSError *error) {}];
 }
-
 
 
 
