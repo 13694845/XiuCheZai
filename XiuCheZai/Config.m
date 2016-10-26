9c7d9a59f8fe540e948f83c73205037c73028bd3
Your Name <you@example.com>
2016-10-26 09:17:19 +0800

    .

--- XiuCheZai/Config.m
+++ XiuCheZai/Config.m
@@ -19,6 +19,7 @@ static NSString *const kVersion = @"1.6.0";
 static NSString *const kWebBaseURL = @"http://m.8673h.com";
 static NSString *const kApiBaseURL = @"http://m.8673h.com";
 static NSString *const kImgBaseURL = @"http://img.8673h.com";
+
 static NSString *const kTestBaseURL = @"http://192.168.2.4:8080";
 static NSString *const kTestImgBaseURL = @"http://192.168.2.4:8888";
 static NSString *const kDomainBaseURL = @"http://a.zj-qsh.com";
@@ -32,7 +33,7 @@ static NSString *const kDomainBaseURL = @"http://a.zj-qsh.com";
 }
 
 + (NSString *)baseURL {
-    return kWebBaseURL;
+    return kTestBaseURL;
 }
 
 + (NSString *)webBaseURL {
@@ -44,7 +45,7 @@ static NSString *const kDomainBaseURL = @"http://a.zj-qsh.com";
 }
 
 + (NSString *)imgBaseURL {
-    return kImgBaseURL;
+    return kTestImgBaseURL;
 }
 
 @end
