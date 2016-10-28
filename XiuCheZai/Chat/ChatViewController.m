d701c2b41927e266b8123028802e74666728e34d
Your Name <you@example.com>
2016-10-28 09:07:39 +0800

    .

--- XiuCheZai/Chat/ChatViewController.m
+++ XiuCheZai/Chat/ChatViewController.m
@@ -352,8 +352,8 @@ typedef NS_ENUM(NSUInteger, InputViewType) {
 
 - (UIView *)textBubbleViewForMessage:(ChatMessage *)message {
     NSAttributedString *attributedText = [ChatEmojiManager emojiStringFromPlainString:message.content withFont:[UIFont systemFontOfSize:14.0]];
-    CGRect TextRect = [attributedText boundingRectWithSize:CGSizeMake(180.0, 20000.0) options:NSStringDrawingUsesLineFragmentOrigin context:nil];
-    UIView *bubbleView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 32.0 + 8.0 + TextRect.size.width + BUBBLE_TEXT_PADDING * 2, TextRect.size.height + BUBBLE_TEXT_PADDING * 2)];
+    CGRect textRect = [attributedText boundingRectWithSize:CGSizeMake(180.0, 20000.0) options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+    UIView *bubbleView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 32.0 + 8.0 + textRect.size.width + BUBBLE_TEXT_PADDING * 2, textRect.size.height + BUBBLE_TEXT_PADDING * 2)];
     
     UIImageView *avatarImageView = [[UIImageView alloc] init];
     if (message.isSend) [avatarImageView sd_setImageWithURL:[NSURL URLWithString:self.senderAvatar]];
@@ -368,11 +368,11 @@ typedef NS_ENUM(NSUInteger, InputViewType) {
     UIView *bubbleImageView = [[UIView alloc] init];
     bubbleImageView.backgroundColor = message.isSend ? [UIColor colorWithRed:221.0/255.0 green:221.0/255.0 blue:221.0/255.0 alpha:1.0] : [UIColor colorWithRed:30.0/255.0 green:130.0/255.0 blue:232.0/255.0 alpha:1.0];
     bubbleImageView.layer.cornerRadius = 5.0;
-    if (message.isSend) bubbleImageView.frame = CGRectMake(32.0 + 8.0, 0.0, TextRect.size.width + BUBBLE_TEXT_PADDING * 2, TextRect.size.height + BUBBLE_TEXT_PADDING * 2);
-    else bubbleImageView.frame = CGRectMake(0.0, 0.0, TextRect.size.width + BUBBLE_TEXT_PADDING * 2, TextRect.size.height + BUBBLE_TEXT_PADDING * 2);
+    if (message.isSend) bubbleImageView.frame = CGRectMake(32.0 + 8.0, 0.0, textRect.size.width + BUBBLE_TEXT_PADDING * 2, textRect.size.height + BUBBLE_TEXT_PADDING * 2);
+    else bubbleImageView.frame = CGRectMake(0.0, 0.0, textRect.size.width + BUBBLE_TEXT_PADDING * 2, textRect.size.height + BUBBLE_TEXT_PADDING * 2);
     [bubbleView addSubview:bubbleImageView];
     
-    UILabel *bubbleText = [[UILabel alloc] initWithFrame:CGRectMake(BUBBLE_TEXT_PADDING, BUBBLE_TEXT_PADDING, TextRect.size.width, TextRect.size.height)];
+    UILabel *bubbleText = [[UILabel alloc] initWithFrame:CGRectMake(BUBBLE_TEXT_PADDING, BUBBLE_TEXT_PADDING, textRect.size.width, textRect.size.height)];
     bubbleText.textColor = message.isSend ? [UIColor colorWithRed:51.0/255.0 green:51.0/255.0 blue:51.0/255.0 alpha:1.0] : [UIColor whiteColor];
     bubbleText.font = [UIFont systemFontOfSize:14.0];
     bubbleText.numberOfLines = 0;
