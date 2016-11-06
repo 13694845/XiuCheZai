f9c841610e1e046ed080c1403719b37fcdf00c98
Your Name <you@example.com>
2016-11-06 13:18:22 +0800

    .

--- XiuCheZai/Chat/ChatViewController.m
+++ XiuCheZai/Chat/ChatViewController.m
@@ -409,14 +409,18 @@ typedef NS_ENUM(NSUInteger, InputViewType) {
     /*
     bubbleImageView.backgroundColor = !message.isSend ? [UIColor colorWithRed:221.0/255.0 green:221.0/255.0 blue:221.0/255.0 alpha:1.0] : [UIColor colorWithRed:30.0/255.0 green:130.0/255.0 blue:232.0/255.0 alpha:1.0];
      */
+    /*
     bubbleImageView.backgroundColor = [UIColor whiteColor];
     bubbleImageView.layer.cornerRadius = 5.0;
+     */
     if (!message.isSend) bubbleImageView.frame = CGRectMake(32.0 + 8.0, 0.0, imageRect.size.width + BUBBLE_TEXT_PADDING * 2, imageRect.size.height + BUBBLE_TEXT_PADDING * 2);
     else bubbleImageView.frame = CGRectMake(0.0, 0.0, imageRect.size.width + BUBBLE_TEXT_PADDING * 2, imageRect.size.height + BUBBLE_TEXT_PADDING * 2);
     [bubbleView addSubview:bubbleImageView];
     
     UIImageView *imgView = [[UIImageView alloc] init];
     /*
+    imgView.layer.borderColor = [UIColor redColor].CGColor;
+    imgView.layer.borderWidth = 1.0;
     imgView.layer.masksToBounds = YES;
     imgView.layer.cornerRadius = 8.0;
      */
