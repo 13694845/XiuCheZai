bf81e5da683ce31cbb9233b3f72a6d270d9520c6
Your Name <you@example.com>
2016-11-06 11:25:10 +0800

    .

--- XiuCheZai/Config.m
+++ XiuCheZai/Config.m
@@ -33,7 +33,7 @@ static NSString *const kDomainBaseURL = @"http://a.zj-qsh.com";
 }
 
 + (NSString *)baseURL {
-    return kWebBaseURL;
+    return kTestBaseURL;
 }
 
 + (NSString *)webBaseURL {
@@ -45,12 +45,11 @@ static NSString *const kDomainBaseURL = @"http://a.zj-qsh.com";
 }
 
 + (NSString *)imgBaseURL {
-    return kImgBaseURL;
+    return kTestImgBaseURL;
 }
 
 + (NSString *)textImgBaseURL {
-    // return kTestImgBaseURL;
-    return kImgBaseURL;
+    return [self imgBaseURL];
 }
 
 @end
