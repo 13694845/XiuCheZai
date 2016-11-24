3bdf0f1c320f568424f1f4e988b9392b38d48e36
Your Name <you@example.com>
2016-11-24 15:56:36 +0800

    .

---
+++ XiuCheZai/Double12AwardViewController.h
@@ -0,0 +1,13 @@
+//
+//  Double12AwardViewController.h
+//  XiuCheZai
+//
+//  Created by QSH on 16/11/24.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Double12AwardViewController : UIViewController
+
+@end
---
+++ XiuCheZai/Double12AwardViewController.m
@@ -0,0 +1,25 @@
+//
+//  Double12AwardViewController.m
+//  XiuCheZai
+//
+//  Created by QSH on 16/11/24.
+//  Copyright © 2016年 QSH. All rights reserved.
+//
+
+#import "Double12AwardViewController.h"
+
+@interface Double12AwardViewController ()
+
+@end
+
+@implementation Double12AwardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
