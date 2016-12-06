a421985ca8bb7ea6cc933f73cbc31141903fd16c
Your Name <you@example.com>
2016-12-06 13:20:26 +0800

    .

--- XiuCheZai/Config.m
+++ XiuCheZai/Config.m
@@ -14,7 +14,7 @@
 
 @implementation Config
 
-static NSString *const kAppVersion = @"1.9.6";
+static NSString *const kAppVersion = @"1.9.7";
 static NSString *const kVersion = @"1.6.0";
 static NSString *const kWebBaseURL = @"http://m.8673h.com";
 static NSString *const kApiBaseURL = @"http://m.8673h.com";
@@ -45,7 +45,7 @@ static NSString *const kDomainBaseURL = @"http://a.zj-qsh.com";
 }
 
 + (NSString *)imgBaseURL {
-    return kImgBaseURL;
+    return kTestImgBaseURL;
 }
 
 + (NSString *)textImgBaseURL {
