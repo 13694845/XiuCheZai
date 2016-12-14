6ff18130f6b851429f684f1f9cfa82dd7de638f1
Your Name <you@example.com>
2016-12-14 14:14:07 +0800

    .

--- XiuCheZai/CachingURLProtocol/CachingURLProtocol.m
+++ XiuCheZai/CachingURLProtocol/CachingURLProtocol.m
@@ -23,18 +23,21 @@ static NSString *const kCachingURLHeader = @"CachingURLHeader";
 @implementation CachingURLProtocol
 
 + (BOOL)canInitWithRequest:(NSURLRequest *)request {
+    /*
     if (![request valueForHTTPHeaderField:kCachingURLHeader]) {
         NSArray *cachingPathExtension = @[@"png", @"jpg", @"JPG", @"jpeg"];
         if ([cachingPathExtension containsObject:request.URL.pathExtension]) {
             return YES;
         }
     }
-    // NSLog(@"URL not cached : %@", request.URL);
+     */
+    NSLog(@"URL not cached : %@", request.URL);
     return NO;
 }
 
 + (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
     // ****************
+    /*
     if ([request.URL.host isEqualToString:@"m.8673h.com"] && [request.URL.description hasPrefix:@"http://"]) {
         NSString *URLString = request.URL.description;
         URLString = [URLString stringByReplacingOccurrencesOfString:@"http://" withString:@"https://"];
@@ -45,6 +48,7 @@ static NSString *const kCachingURLHeader = @"CachingURLHeader";
         URLString = [URLString stringByReplacingOccurrencesOfString:@"http://" withString:@"https://"];
         request = [NSURLRequest requestWithURL:[NSURL URLWithString:URLString]];
     }
+     */
     return request;
 }
 
