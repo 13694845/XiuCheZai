fbc6c9b33656670de6a73080103a9746cd30037e
Your Name <you@example.com>
2016-12-16 08:57:18 +0800

    .

--- XiuCheZai/XCZHomeViewController.m
+++ XiuCheZai/XCZHomeViewController.m
@@ -22,21 +22,6 @@
     [super viewDidLoad];
     self.tabBarController.tabBar.tintColor = [UIColor colorWithRed:229.0/255.0 green:21.0/255.0 blue:45.0/255.0 alpha:1.0];
     self.scrollView.backgroundColor = [UIColor lightGrayColor];
-    /*
-    self.scrollView.showsVerticalScrollIndicator = NO;
-    self.scrollView.contentSize = self.mainView.bounds.size;
-     */
-    // NSLog(@"scrollView : %@", NSStringFromCGRect(self.scrollView.frame));
-}
-
-- (void)viewWillLayoutSubviews {
-    [super viewWillLayoutSubviews];
-    // NSLog(@"scrollView : %@", NSStringFromCGRect(self.scrollView.frame));
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    // NSLog(@"scrollView : %@", NSStringFromCGRect(self.scrollView.frame));
 }
 
 - (void)didReceiveMemoryWarning {
