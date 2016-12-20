079758826aa20c3f6db92ee73ff1e6ebd1a2a93f
Your Name <you@example.com>
2016-12-20 14:47:58 +0800

    .

--- XiuCheZai/WebViewController.m
+++ XiuCheZai/WebViewController.m
@@ -144,7 +144,7 @@
     }
     
     if ([request.URL.description containsString:[NSString stringWithFormat:@"%@%@", [Config baseURL], @"/m-center/my_car/index.html"]]) {
-        sleep(0.5);
+        sleep(2.0);
         return YES;
     }
     
